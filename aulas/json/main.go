913e021d0eddb156e7b723a9476d477206bdde84
HeitorLoureiro1611 <heitor.loureiro1611@gmail.com>
2026-04-21 11:14:22 -0300

    [feat] Metodo unmarshal de json

--- aulas/json/main.go
+++ aulas/json/main.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 )
 
+// Go diferencia Publico de Privado com a primeira letra em maiúsculo
+// Pessoa > publico
+// pessoa > privado
 type Pessoa struct {
 	Nome      string
 	Sobrenome string
@@ -15,14 +18,33 @@ type Pessoa struct {
 func main() {
 	Cliente := Pessoa{"Delo", "Phantae", 26}
 
+	var cliente2 Pessoa
+
 	// b -> []bytes, err -> erro
 	// marshal é a ordenação em json
-	b, err := json.Marshal(Cliente)
+	b, err1 := json.Marshal(Cliente)
 	// caso tenha algum erro, mostre o erro
-	if err != nil {
-		fmt.Println(err)
+	if err1 != nil {
+		fmt.Println(err1)
 	}
 	// ver o objeto em uma formatação json
 	// é necessario parsear como string pois é um []bytes
 	fmt.Println(string(b))
+
+	// ##################################################################
+	// Unmarshal e decodificando JSON
+
+	// json é tratado como []bytes
+	un := []byte(`{"Nome":"Delo","Sobrenome":"Phantae","Idade":26}`)
+
+	// declaração do unmarshal
+	// erro vai ser gerado
+	// Unmarshal([]bytes, &var)
+	err := json.Unmarshal(un, &cliente2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// não é lidado como uma variável decodificada de json
+	fmt.Println(cliente2)
+
 }
